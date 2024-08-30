2f35f6ac8b8ebb2231d429b143475e1151ff0851
shinnosuke <kawaguchi.shin.03@gmail.com>
2024-08-30 15:57:14 +0900

    アニメーションの追加

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ const swiper = new Swiper(".mySwiper", {
   },
 });
 
+// ハンバーガーメニュー
 const $humBtn = document.getElementById("js-hum");
 const $spMenu = document.querySelector(".p-sp__menu");
 const $bodyElemnt = document.body;
@@ -22,3 +23,82 @@ function humToggle() {
   $spMenu.classList.toggle("is-active");
   $bodyElemnt.classList.toggle("active-body");
 }
+
+// スプリット文字の実装とアニメーション
+const $textEl = document.getElementById("js-split");
+if ($textEl) {
+  const htmlContent = $textEl.innerHTML;
+  const splitText = htmlContent
+    .replace(/(<br>|<br \/>)/gi, "|")
+    .split("")
+    .map((char) => (char === "|" ? "<br>" : `<span>${char}</span>`))
+    .join("");
+  $textEl.innerHTML = splitText;
+}
+// タイムライン制御
+const tl = gsap.timeline();
+tl.fromTo(
+  ".el1",
+  {
+    autoAlpha: 0,
+    y: 50,
+  },
+  {
+    autoAlpha: 1,
+    y: 0,
+    duration: 2,
+  }
+)
+  .fromTo(
+    "#js-split span",
+    {
+      autoAlpha: 0,
+      y: 20,
+    },
+    {
+      autoAlpha: 1,
+      y: 0,
+      stagger: 0.1,
+      duration: 0.6,
+    }
+  )
+  .fromTo(
+    ".el3",
+    {
+      autoAlpha: 0,
+      y: 20,
+    },
+    {
+      autoAlpha: 1,
+      y: 0,
+    }
+  )
+  .fromTo(
+    ".el4",
+    {
+      autoAlpha: 0,
+      y: 20,
+    },
+    {
+      autoAlpha: 1,
+      y: 0,
+    }
+  );
+
+// フェードインアニメーション
+gsap.utils.toArray(".fade").forEach((target) => {
+  gsap.from(target, {
+    duration: 0.8,
+    scrollTrigger: {
+      trigger: target,
+      start: "top bottom",
+      // markers: true,
+    },
+    stagger: {
+      from: "start",
+      amount: 0.8,
+    },
+    opacity: 0,
+    yPercent: 20,
+  });
+});
